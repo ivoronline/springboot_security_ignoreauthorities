7a8d9efcfcceeeaa0241b8011bc6797b34a39cea
ivoronline <ivoronline@gmail.com>
2022-01-20 21:09:58 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_security_ignoreauthorities/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_ignoreauthorities/config/WebSecurityConfig.java
@@ -1,32 +1,11 @@
 package com.ivoronline.springboot_security_ignoreauthorities.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-  //=================================================================
-  // CONFIGURE (Auth...)
-  //=================================================================
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("myuser" ).password("myuserpassword" ).roles("USER" );
-    auth.inMemoryAuthentication().withUser("myadmin").password("myadminpassword").roles("ADMIN");
-  }
-
-  //=======================================================================
-  // PASSWORD ENCODER
-  //=======================================================================
-  @Bean
-  PasswordEncoder passwordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
-  }
 
   //=================================================================
   // CONFIGURE
